95ad95a423a5a3d2bcea28231a0483b7d1014be0
JeongAnNa <rib040211@gmail.com>
2022-07-11 10:26:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const expressSession = require('express-session');
+const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
+const http = require('http');
+const con = require('./bin/db');
+__dirname = path.resolve();
+require('dotenv').config();
 
 const route = {
   indexRouter: require('./routes/index'),
-}
-const port = 3000;
+  loginRouter: require('./routes/login'),
+  joinRouter: require('./routes/join'),
 
-var app = express();
+}
+const port = 8080;
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(serveStatic(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+
+con.connect(function(err){
+  if(err) throw err;
+  console.log("DB Connected!!!");
+});
+
+module.exports = con;
+
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 app.use('/', route.indexRouter);
+app.use('/login', route.loginRouter);
+app.use('/join', route.joinRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,7 +70,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 app.listen(port, () => console.log(`part-time-job app listening on port ${port}`));
 
